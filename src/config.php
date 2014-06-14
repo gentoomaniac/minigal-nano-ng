5e2d1bd0792e62b5a4b2b61259f838304374f371
Marco Siebecke <gentoomaniac@gmail.com>
2014-06-14 02:23:59 +0200

    small images for webview are created and cached

--- src/config.php
+++ src/config.php
@@ -38,6 +38,7 @@ $label_loading          = "Loading..."; //Thumbnail loading text
 
 //ADVANCED SETTINGS
 $thumb_size             = 120; //Thumbnail height/width (square thumbs). Changing this will most likely require manual altering of the template file to make it look properly!
+$small_size             = 1024;
 $label_max_length       = 30; //Maximum chars of a folder name that will be displayed on the folder thumbnail
 $display_exif           = 1;
 
