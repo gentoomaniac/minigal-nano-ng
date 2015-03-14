e79102bb81fc09b7f3afaa261a66677aaa9c428f
Denis Shulyaka <Shulyaka@gmail.com>
2015-03-14 23:56:18 +0300

    Russian translation
    Signed-off-by: Denis Shulyaka <Shulyaka@gmail.com>

---
+++ src/i18n/ru_RU.php
@@ -0,0 +1,38 @@
+<?php
+/*
+MINIGAL NANO
+- A PHP/HTML/CSS based image gallery script
+
+This script and included files are subject to licensing from Creative Commons (http://creativecommons.org/licenses/by-sa/2.5/)
+You may use, edit and redistribute this script, as long as you pay tribute to the original author by NOT removing the linkback to www.minigal.dk ("Powered by MiniGal Nano x.x.x")
+
+MiniGal Nano is created by Thomas Rybak
+
+Copyright 2010 by Thomas Rybak
+Support: www.minigal.dk
+Community: www.minigal.dk/forum
+
+Please enjoy this free script!
+*/
+
+if(! defined("MINIGAL_INTERNAL")) {
+    exit;
+}
+
+//LANGUAGE STRINGS
+$i18n['label_home']             = "Альбомы"; //Name of home link in breadcrumb navigation
+$i18n['label_new']              = "New"; //Text to display for new images. Use with $display_new variable
+$i18n['label_page']             = "Страница"; //Text used for page navigation
+$i18n['label_all']              = "Всё"; //Text used for link to display all images in one page
+$i18n['label_noimages']         = "В этом альбоме нет фотографий"; //Empty folder text
+$i18n['label_loading']          = "Загрузка..."; //Thumbnail loading text
+
+$i18n['msg_update_available']   = "Доступно обновление MiniGal Nano NG %s!";
+$i18n['msg_first_run']          = "Судя по всему, Вы только что установили MiniGal Nano NG. Пожалуйста, запустите <a href='system_check.php'>проверку системной конфигурации</a>";
+
+$i18n['error_loading_internal'] = "Этот файл не предполагается вызывать непосредственно!";
+$i18n['error_no_exif_support']  = "Error: Расширение PHP EXIF не доступно. Установите &#36;display_exif = 0; в config.php, чтобы убрать это сообщение";
+$i18n['error_file_permissions'] = "Как минимум у одного файла или директории недостаточные права доступа";
+
+return "MINIGAL_INCLUDE_OK";
+?>
