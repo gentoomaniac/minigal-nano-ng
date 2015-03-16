25728da04e9f9492105db76303d35bc87a7e647e
Marco Siebecke <gentoomaniac@gmail.com>
2015-03-16 13:49:53 +0100

    Added config check and i18n support to system_check.php

--- src/i18n/en_US.php
+++ src/i18n/en_US.php
@@ -34,5 +34,21 @@ $i18n['error_loading_internal'] = "This file should not be loaded directly!";
 $i18n['error_no_exif_support']  = "Error: PHP EXIF is not available. Set &#36;display_exif = 0; in config.php to remove this message";
 $i18n['error_file_permissions'] = "At least one file or folder has wrong permissions";
 
+$i18n['syscheck_page_title']    = "MiniGal Nano NG system check";
+$i18n['syscheck_php_title']     = "PHP version";
+$i18n['syscheck_php_desc']      = '<a href="http://www.php.net/" target="_blank">PHP</a> scripting language version 4.0 or greater is needed';
+$i18n['syscheck_gd_title']      = 'GD library support';
+$i18n['syscheck_gd_desc']       = '<a href="http://www.boutell.com/gd/" target="_blank">GD image manipulation</a> library is used to create thumbnails. Bundled since PHP 4.3';
+$i18n['syscheck_exif_title']    = 'EXIF support';
+$i18n['syscheck_exif_desc']     = 'Ability to extract and display <a href="http://en.wikipedia.org/wiki/Exif" target="_blank">EXIF information</a>. The script will work without it, but not display image information';
+$i18n['syscheck_videothumb_title'] = 'Video thumbnail support';
+$i18n['syscheck_videothumb_desc']  = '<a href="https://code.google.com/p/ffmpegthumbnailer/" target="_blank">ffmpgthumbnailer</a> is used to create thumbnails of supported video files.';
+$i18n['syscheck_conf_title']    = 'Configuration';
+$i18n['syscheck_conf_desc']     = 'Check if configuration file exists and the config file is includable.';
+$i18n['syscheck_mem_title']     = 'PHP memory limit';
+$i18n['syscheck_mem_desc']      = 'Memory is needed to create thumbnails. Bigger images uses more memory';
+$i18n['syscheck_versioncheck_title'] = 'New version check';
+$i18n['syscheck_versioncheck_desc']  = 'The ability to check for new version and display this automatically. The script will work without it';
+
 return "MINIGAL_INCLUDE_OK";
 ?>
